0a04756f1130fcdae73876edb55e8cf6aed17888
dunky-star <dunkygeoffrey39@gmail.com>
2024-08-24 14:49:23 -0500

    Created CRUD unit tests

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:cluster@1@localhost:5432/paseto_auth?sslmode=disable"
+)
+
+var testQueries *Queries // Deifining it as a global variable since we shall use it extensively in all unit tests
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
