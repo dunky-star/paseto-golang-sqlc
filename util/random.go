83bb898646838a7315da0a058c5d1c1753f53ab6
dunky-star <dunkygeoffrey39@gmail.com>
2024-08-26 15:14:04 -0500

    Completed implementing password hashing with BCrypt

--- util/random.go
+++ util/random.go
@@ -27,6 +27,11 @@ func RandomEmail() string {
 	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
 
+// RandomOwner generates a random owner name
+func RandomOwner() string {
+	return RandomString(6)
+}
+
 // RandomString generates a random string of length n
 func RandomString(n int) string {
 	var sb strings.Builder
